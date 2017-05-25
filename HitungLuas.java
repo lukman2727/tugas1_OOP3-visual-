a55c6fc9fda3b55f9522e3126bcd7c536dd6a653
Lukman Kurniawan <campione47@gmail.com>
2017-05-25 09:35:41 +0700

    Luas Lingkaran

--- HitungLuas.java
+++ HitungLuas.java
@@ -1,4 +1,7 @@
 class HitungLuas{
+	public static float cariLuasLingkaran(int r){
+		return (22/7) * r * r;
+	}
 	public static int cariLuasPersegiPanjang(int panjang, int lebar){
 		return panjang * lebar;
 	}
